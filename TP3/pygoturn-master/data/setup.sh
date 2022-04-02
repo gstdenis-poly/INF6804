6603d3f18c1c6503672d012cdad68e1eae9c003d
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2022-04-02 13:35:45 -0400

    Only consider alov300 dataset in setup.sh

--- TP3/pygoturn-master/data/setup.sh
+++ TP3/pygoturn-master/data/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # setup imagenet images 
-tar -xvf ILSVRC2014_DET_train.tgz
-cp ./untar.sh ./ILSVRC2014_DET_train/ && cd ./ILSVRC2014_DET_train/
+# tar -xvf ILSVRC2014_DET_train.tgz
+# cp ./untar.sh ./ILSVRC2014_DET_train/ && cd ./ILSVRC2014_DET_train/
 # delete ILSVRC2013* data to exactly match GOTURN training images 
-rm -rf ILSVRC2013*
-./untar.sh
-rm -rf *.tar
-rm untar.sh && cd ..
+# rm -rf ILSVRC2013*
+# ./untar.sh
+# rm -rf *.tar
+# rm untar.sh && cd ..
 
 # setup imagenet bounding boxes
-tar -xvf ILSVRC2014_DET_bbox_train.tgz
+# tar -xvf ILSVRC2014_DET_bbox_train.tgz
 
 # setup alov images + bounding boxes
 unzip alov300++_frames.zip
